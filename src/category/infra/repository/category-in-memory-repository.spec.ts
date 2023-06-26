9809c237867535f2b591ba7a94f3e4094c0edfc7
Leonardo de Faveri <faver_i@hotmail.com>
2023-06-26 16:43:18 -0300

    implements tests and feat in memory category repository

---
+++ src/category/infra/repository/category-in-memory-repository.spec.ts
@@ -0,0 +1,139 @@
+import Category from "../../domain/entities/category";
+import CategoryRepository from "../../domain/repository/category-repository";
+import CategoryInMemoryRepository from "./category-in-memory-repository";
+
+describe("CategoryInMemoryRepository Unit Tests", () => {
+  let categoryInMemoryRepository: CategoryInMemoryRepository;
+
+  beforeEach(() => {
+    categoryInMemoryRepository = new CategoryInMemoryRepository();
+  });
+
+  test("should no filter items when param is null", async () => {
+    const categories = [new Category({ name: "c" })];
+    const itemsWithNoFilter = await categoryInMemoryRepository["applyFilter"](
+      categories,
+      null
+    );
+    expect(itemsWithNoFilter).toStrictEqual(categories);
+  });
+  test("should filter items per name with any string term from start to end of name value", async () => {
+    const categories = [
+      new Category({ name: "teste" }),
+      new Category({ name: "Teste" }),
+      new Category({ name: "any name" }),
+      new Category({ name: "TESTE" }),
+      new Category({ name: "tEStE" }),
+    ];
+    let itemsWithFilter = await categoryInMemoryRepository["applyFilter"](
+      categories,
+      "teste"
+    );
+    expect(itemsWithFilter).toStrictEqual([
+      categories[0],
+      categories[1],
+      categories[3],
+      categories[4],
+    ]);
+    itemsWithFilter = await categoryInMemoryRepository["applyFilter"](
+      categories,
+      "am"
+    );
+    expect(itemsWithFilter).toStrictEqual([categories[2]]);
+  });
+  test("should sort default the categories by createdAt desc", async () => {
+    const createdAt = new Date();
+    const categories = [
+      new Category({
+        name: "teste",
+        createdAt: new Date(createdAt.getTime() + 50),
+      }),
+      new Category({
+        name: "Teste",
+        createdAt: new Date(createdAt.getTime() + 100),
+      }),
+      new Category({
+        name: "any name",
+        createdAt: new Date(createdAt.getTime() + 300),
+      }),
+      new Category({
+        name: "TESTE",
+        createdAt: new Date(createdAt.getTime() + 200),
+      }),
+      new Category({
+        name: "tEStE",
+        createdAt: new Date(createdAt.getTime() + 400),
+      }),
+    ];
+    let itemsFiltered = await categoryInMemoryRepository["applySort"](
+      categories,
+      null,
+      null
+    );
+    expect(itemsFiltered).toStrictEqual([
+      categories[4],
+      categories[2],
+      categories[3],
+      categories[1],
+      categories[0],
+    ]);
+  });
+  test("should sort the categories ignoring by default createdAt desc", async () => {
+    const createdAt = new Date();
+    const categories = [
+      new Category({
+        name: "teste",
+        createdAt: new Date(createdAt.getTime() + 50),
+      }),
+      new Category({
+        name: "Teste",
+        createdAt: new Date(createdAt.getTime() + 100),
+      }),
+      new Category({
+        name: "Any name",
+        createdAt: new Date(createdAt.getTime() + 300),
+      }),
+      new Category({
+        name: "TESTE",
+        createdAt: new Date(createdAt.getTime() + 200),
+      }),
+      new Category({
+        name: "tEStE",
+        createdAt: new Date(createdAt.getTime() + 400),
+      }),
+    ];
+    let itemsFiltered = await categoryInMemoryRepository["applySort"](
+      categories,
+      "name",
+      "asc"
+    );
+    expect(itemsFiltered).toStrictEqual([
+      categories[2],
+      categories[3],
+      categories[1],
+      categories[4],
+      categories[0],
+    ]);
+  });
+  test("should not sort if has not sortableFields property", async () => {
+    const categories = [
+      new Category({ name: "teste" }),
+      new Category({ name: "Teste" }),
+      new Category({ name: "Any name" }),
+      new Category({ name: "TESTE" }),
+      new Category({ name: "tEStE" }),
+    ];
+    categoryInMemoryRepository["items"] = categories;
+    const searchParams = new CategoryRepository.SearchParams({
+      sort: "name",
+    });
+    let itemsFiltered = await categoryInMemoryRepository.search(searchParams);
+    expect(itemsFiltered.items).toStrictEqual([
+      categories[2],
+      categories[3],
+      categories[1],
+      categories[4],
+      categories[0],
+    ]);
+  });
+});
--- src/category/infra/repository/category-in-memory-repository.ts
+++ src/category/infra/repository/category-in-memory-repository.ts
@@ -1,18 +1,30 @@
+import { SortDirection } from "@seedwork/domain/repository/search-params";
 import Category from "category/domain/entities/category";
 import CategoryRepository from "category/domain/repository/category-repository";
 import InMemorySearchableRepository from "../../../@seedwork/domain/repository/in-memory-searchable-repository";
 
-class CategoryInMemoryRepository
+export default class CategoryInMemoryRepository
   extends InMemorySearchableRepository<Category>
   implements CategoryRepository.Repository
 {
+  protected sortableFields: string[] = ["name", "createdAt"];
+
   protected async applyFilter(
     items: Category[],
-    filter: CategoryRepository.Filter
+    filter: string
   ): Promise<Category[]> {
-    if(!filter) return items
+    if (!filter) return items;
     return items.filter((item) => {
-      return item.props.name.toLowerCase().includes(filter.toLowerCase())
-    })
+      return item.name.toLowerCase().includes(filter.toLowerCase());
+    });
+  }
+  protected async applySort(
+    items: Category[],
+    sort: string,
+    sortDir: SortDirection
+  ): Promise<Category[]> {
+    return !sort
+      ? super.applySort(items, "createdAt", "desc")
+      : super.applySort(items, sort, sortDir);
   }
 }
